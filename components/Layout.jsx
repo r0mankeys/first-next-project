72bb0045d5373d5260eca18f0809308b0b8cdcec
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2023-06-05 08:27:12 +0100

    Updated siteTitle

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.sass";
 import Link from "next/link";
 
 const name = "Safia Worthing";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Home Page";
 
 export default function Layout({ children, home }) {
   return (
